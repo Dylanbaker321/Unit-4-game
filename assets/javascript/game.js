8a8145445c5d3fa54e3265848082580c603b5daf
Dylan Baker <Dylanbaker321@gmail.com>
2019-08-24 19:51:04 -0700

    <finsihed basic html added vars for crystals wins losses and user totals>

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,2 +1,26 @@
 console.log("Test");
 
+
+$(document).ready(function() {
+
+//This sets the goal number
+var targetNumber = Math.floor(Math.random() * 120) + 20;
+$("#pointTotal").text(targetNumber);
+//Variables
+var CryOne = Math.floor(Math.random()*11+1)
+var CryTwo= Math.floor(Math.random()*11+1)
+var CryThree= Math.floor(Math.random()*11+1)
+var CryFour= Math.floor(Math.random()*11+1)
+
+var userTotal= 0; 
+var wins= 0;
+var loses = 0;
+
+// Shows the number of times you have won and lost
+$("#wins").text("you have won " + wins + " Times");
+$("#loses").text("you have lost " + loses + " Times");
+
+
+
+
+});
